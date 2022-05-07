7a5287389f995941a4400db96f5746985166844d
matthewdjb <23376187+matthewdjb@users.noreply.github.com>
2022-05-07 16:41:38 +0200

    icons

--- com.xiting.a4e.ui/src/com/xiting/a4e/ui/launch/LaunchShortcut.java
+++ com.xiting.a4e.ui/src/com/xiting/a4e/ui/launch/LaunchShortcut.java
@@ -11,12 +11,14 @@ import com.ixiting.a4e.backend.IAlchemistRunner;
 public class LaunchShortcut implements ILaunchShortcut {
 	@Override
 	public void launch(ISelection selection, String mode) {
+		// Runs from Outline context
 		IAlchemistRunner runner = AlchemistRunnerFactory.get();
 		runner.run(selection);
 	}
 
 	@Override
 	public void launch(IEditorPart editorPart, String mode) {
+		// Runs from editor context
 		MultiPageEditorPart editor = (MultiPageEditorPart) editorPart;
 		IAlchemistRunner runner = AlchemistRunnerFactory.get();
 		runner.run(editor);
